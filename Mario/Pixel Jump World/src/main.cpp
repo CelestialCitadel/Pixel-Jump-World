0f7539155b357c1fac5a774dcfc2308b0aca04d0
Anish Biswal <biswalworkanish@gmail.com>
2024-02-03 19:35:46 +0530

    added the sfml libraries

---
+++ Mario/Pixel Jump World/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void main() {
+	std::cout << "Hello Coders";
+	std::cout << "Finally its working";
+	std::cout << "Hello Anish and Deepanshu!";
+	std::cin.get();
+	
+}
--- Pixel Jump World/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-void main() {
-	std::cout << "Hello Coders";
-	std::cout << "Finally its working";
-	std::cout << "Hello Anish and Deepanshu!";
-	std::cin.get();
-	
-}
