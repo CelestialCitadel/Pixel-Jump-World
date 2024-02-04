f3211a49eebedd6af4893234e2c1b8c60e89849a
deepencoding <deepanshugautam12a13@gmail.com>
2024-02-04 14:29:00 +0530

    - Setup window - Made the window framerate independent

---
+++ Mario/Pixel Jump World/headers/common.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+// blocks
+static constexpr unsigned char CELL_SIZE = 16;
+static constexpr unsigned char FONT_HEIGHT = 16;
+static constexpr unsigned char MAP_WIDTH = 21;
+static constexpr unsigned char MAP_HEIGHT = 21;
+
+static constexpr unsigned char TEXTURE_BIT = 16;
+
+static constexpr unsigned char SCREEN_RESIZE_FACTOR = 2;
+
+static constexpr unsigned short SCREEN_WIDTH = 256;
+static constexpr unsigned short SCREEN_HEIGHT = 240;
+
+static constexpr unsigned short FRAME_DURATION = 16667;
--- Mario/Pixel Jump World/src/main.cpp
+++ Mario/Pixel Jump World/src/main.cpp
@@ -1,23 +1,60 @@
+#include <chrono>
 #include <SFML/Graphics.hpp>
+
+#include "../headers/common.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH * SCREEN_RESIZE_FACTOR, SCREEN_HEIGHT * SCREEN_RESIZE_FACTOR), "Super Mario Bros!");
+    //window.setView(sf::View(sf::FloatRect(0, -CELL_SIZE, CELL_SIZE * MAP_WIDTH, FONT_HEIGHT + (CELL_SIZE + 1) * MAP_HEIGHT)));
 
+    sf::CircleShape circle(50.f);
+    circle.setFillColor(sf::Color::Red);
+    circle.setPosition(sf::Vector2f(0.f, 0.f));
+
+    unsigned lag = 0;
+    std::chrono::time_point<std::chrono::steady_clock> prev_time;
+
+    prev_time = std::chrono::steady_clock::now();
+
+    // ========================= Main game loop =========================
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+        unsigned dt = (unsigned)std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - prev_time).count();
+        lag += dt;
+        prev_time += std::chrono::microseconds(dt);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        sf::Event event;
+        while (lag >= FRAME_DURATION)
+        {
+            lag -= FRAME_DURATION;
+
+            // ========================= Event loop =========================
+            while (window.pollEvent(event))
+            {
+                switch (event.type)
+                {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+
+                default:
+                    break;
+                }
+            }
+            // ========================= Update game state =========================
+
+
+            if (FRAME_DURATION > lag)
+            {
+                window.clear();
+                // ========================= Render or Draw =========================
+                window.draw(circle);
+                window.display();
+
+            }
+
+        }
     }
 
     return 0;
